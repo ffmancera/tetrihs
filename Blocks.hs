fae61acf0d139dc065737350ad84dfbcc7612d7c
Fernando <ffmancera@riseup.net>
2018-11-23 21:49:36 +0100

    Add main modules to the repository.
    Add main modules to the repository even if they are empty.
    
    Signed-off-by: Fernando <ffmancera@riseup.net>

--- Blocks.hs
+++ Blocks.hs
@@ -1,14 +1,51 @@
 -- This file contains all the functions related to all the tetris pieces.
 module Blocks where
 import CodeWorld
+import System.Random
 
 {- Every piece is represented as a matrix where 1's are represented with a
  - non-white block. 0's are represented with a white block. The matrices have a
- - size of 4x2 blocks.: -}
+ - size of 2x4 blocks.: -}
 
-type Block = Maybe Colour
+type Block = Integer
 data Colour = Black | Yellow | Green | Blue | Pink | Purple | Red | Orange
 
 {- Define the matrix and row type -}
-type row = [Block]
-type matrix = [row]
+type Row = [Block]
+type Matrix = [Row]
+
+{- Define all the tetromino pieces -}
+figureSet :: [Matrix]
+figureSet = [ [[0,0,0,0],
+               [1,1,1,1]],
+
+              [[0,0,1,1],
+               [0,0,1,1]],
+
+              [[0,1,1,1],
+               [0,0,1,0]],
+
+              [[0,1,1,1],
+               [0,0,0,1]],
+
+              [[0,1,1,1],
+               [0,1,0,0]],
+
+              [[0,0,1,1],
+               [0,1,1,0]],
+
+              [[0,1,1,0],
+               [0,0,1,1]] ]
+
+{- This function counts how many white blocks are in a specific matrix. -}
+countNonWhiteBlocks :: Matrix -> Integer
+countNonWhiteBlocks matrix
+    | length matrix == 0 = 0
+    | otherwise = sum (head matrix) + countNonWhiteBlocks (tail matrix)
+
+{- This function picks a random color from the color list -}
+pickRandomColor :: [Color] -> Color
+pickRandomColor colorList = do
+    let gen = RandomGen.newStdGen
+    let randelem = take 1 (randomRs (0, ((length colorList)-1)) gen)
+    colorList !! head randelem
