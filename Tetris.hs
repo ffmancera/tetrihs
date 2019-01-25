693971b35ae0f7f0d5c9f158a08865287ee846a9
Fernando <ffmancera@riseup.net>
2019-01-25 22:26:08 +0100

    Changes in the Tetris.hs module.
    Most of the types has been moved to the CodeWorldInterface module.

--- Tetris.hs
+++ Tetris.hs
@@ -1,22 +1,17 @@
-module Main where
+module Tetris where
 import Blocks
 import CodeWorld
+import CodeWorldInterface
 import Scores
 
-main = print "hehe"
-
-{- Some types that we need to make the game state easy to understand. -}
-type Figure = (Matrix, Color)
-type Board = Figure
-type Position = (Int, Int)
-
-{- Game state, we have the next elements:
- - Current Figure: We apply the actions over this figure.
- - Board: Static 10x20 empty Matrix
- - FigureSet: All the figures in the board -}
-data TetrisGame = TG (Figure) Board [Figure]
-
-{- Game board with a fixed length. To be honest, this is only used as a
- - reference but I find it awesome anyway.-}
-boardGame :: Matrix
-boardGame = replicate 20 (replicate 10 0)
+{- List of pieces. -}
+initialMatrixes = foldr (++) [] (replicate 400 figureSet)
+initialColors = foldr (++) [] (replicate 400 colorSet)
+aLotOfFigures = zip figureSet colorSet
+{- Main thread of the game. -}
+tetris :: Int -> IO()
+tetris level = do
+	let initialFigure = (head (aLotOfFigures))
+	let initialState = TG initialFigure [] (tail aLotOfFigures) level 0
+	print "Game ready! Press Any Key in http://locahost:3000 to start"		
+	drawTetris initialState
